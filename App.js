ff3ed6f76527ee59c64f16c1eede2e6a290bebfd
Maxe <maxe7349@gmail.com>
2019-10-15 23:36:28 +0200

    Revert "added buttonHandler for number buttons and operator button (C)"
    This reverts commit 72ec26e1a3f3a6379dd57926f9817cc61e6757f2.

--- App.js
+++ App.js
@@ -6,72 +6,32 @@ export default class App extends Component {
   constructor(){
     super();
     this.state = {
-      resultText: "", 
+      show: true
     };
   }
 
-  opButtonHandler(buttonText){
-    switch(buttonText){
-      case 'C':
-        const term = this.state.resultText.split('')
-        console.log(term)
-        term.pop()
-        this.setState({
-          resultText: term.join('')
-        }) 
-    }
-  }
-
-  numButtonHandler(buttonText){
-    console.log(this.state.resultText)
-    if(this.state.resultText === "NaN"){
-      this.setState({
-        resultText : buttonText.toString()
-      })
-      return 
-    }
-    else if(buttonText === '='){
-      return this.calcRst()
-    }else {
-      const sum = this.state.resultText + buttonText 
-      this.setState({
-        resultText : sum
-      })
-    }
-  }
-
-  calcRst(){
-    const input = this.state.resultText 
-    this.setState({
-      resultText: "NaN"
-    }) 
-    /**@todo split input and calc result */
-  }
-
   render(){
-
+    
     let numElems = []  //init html elements to print the button grid
     let items = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [',', 0, '=']]
     for(let i = 0; i < 4; i++){
       let row = []
       for(let j = 0; j < 3; j++){
-        row.push(<TouchableOpacity style={styles.touchable} onPress={() => this.numButtonHandler(items[i][j])}><Text style={styles.btnText}>{items[i][j]}
+        row.push(<TouchableOpacity style={styles.touchable}><Text style={styles.btnText}>{items[i][j]}
         </Text></TouchableOpacity>)
       }
       numElems.push(<View style={styles.row}>{row}</View>)
     }
 
-    let operators = ['C', '+', '-', '*', '/']
+    let operators = ['+', '-', '*', '/']
     let opElems = [];
-    for(let i = 0; i < 5; i++){
-      opElems.push(<TouchableOpacity style={styles.touchable} onPress={() => this.opButtonHandler(operators[i])}><Text style={styles.btnText}>{operators[i]}</Text></TouchableOpacity>)
+    for(let i = 0; i < 4; i++){
+      opElems.push(<TouchableOpacity style={styles.touchable}><Text style={styles.btnText}>{operators[i]}</Text></TouchableOpacity>)
     }
     
     return (
       <View style={styles.container}>
-        <View style={styles.inputs}><Text style={styles.rstText}>
-        {this.state.resultText}
-        </Text>
+        <View style={styles.inputs}>
         </View>
         <View style={styles.result}>
         </View>
@@ -96,8 +56,6 @@ const styles = StyleSheet.create({
   inputs: {
     flex: 2, 
     backgroundColor: 'red', 
-    justifyContent: 'center', 
-    alignItems: 'flex-end', 
   }, 
   result: {
     flex: 1, 
@@ -131,8 +89,5 @@ const styles = StyleSheet.create({
   }, 
   btnText: {
     fontSize: 30
-  }, 
-  rstText: {
-    fontSize: 75
   }
 });
